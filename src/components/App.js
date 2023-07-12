8972b4719e0c53ebf890855e9499a96f07064dd9
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-07-12 15:08:20 -0400

    Setup components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Component } from "react";
+import Overview from "./Overview";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <>
+        <form>
+          <input type="text"></input>
+          <button>Add Task</button>
+        </form>
+
+        <Overview />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Overview extends Component {
+  render() {
+    return <div className="tasks">Overview</div>;
+  }
+}
+
+export default Overview;
